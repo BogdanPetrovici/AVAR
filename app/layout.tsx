cea0b81e95ff4c7a3bb82ca334e8dd6c38909b7e
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-05-04 13:55:17 +0300

    Changed layout scheme, in order to remove sidebar navigation from login page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import '@/app/ui/css/global.css';
 import styles from '@/app/ui/css/layout.module.css';
 import { inter } from '@/app/ui/fonts';
-import SideNav from '@/app/ui/sidenav';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { Toaster } from 'react-hot-toast';
 
 export default function RootLayout({
   children,
@@ -13,15 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${styles.bodyText}`}>
-        <AppRouterCacheProvider>
-          <div className={styles.appContainer}>
-            <div className={styles.navContainer}>
-              <SideNav />
-            </div>
-            <div className={styles.tabContainer}>{children}</div>
-          </div>
-        </AppRouterCacheProvider>
-        <Toaster position="bottom-center" />
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
   );
