c42572b7494ed2f8e064594af541fa5f4666acea
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-05-14 14:54:20 +0300

    Updated login page to redirect to Cognito OAuth provider.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,12 +4,10 @@ import styles from '@/app/ui/css/login.module.css';
 
 export default function LoginPage() {
   return (
-    <main className={styles.pageContainer}>
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
+    <main className={styles.pageCanvas}>
+      <div className={styles.pageContainer}>
+        <div className={styles.pageHeading}>
+          <AcmeLogo />
         </div>
         <LoginForm />
       </div>
--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,85 +1,38 @@
-'use client';
-
 import { lusitana } from '@/app/ui/fonts';
-import {
-  AtSymbolIcon,
-  KeyIcon,
-  ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@mui/material';
-import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
 import styles from '@/app/ui/css/login.module.css';
+import { signIn } from '@/auth';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
-
   return (
-    <form action={dispatch} className={styles.loginForm}>
-      <div className={styles.loginContainer}>
-        <h1 className={`${lusitana.className} ${styles.loginHeading}`}>
-          Please log in to continue.
-        </h1>
-        <div className={styles.loginContent}>
-          <div>
-            <label className={styles.loginLabel} htmlFor="email">
-              Email
-            </label>
-            <div className={styles.loginFieldContainer}>
-              <input
-                className={styles.loginField}
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSymbolIcon className={styles.loginFieldIcon} />
-            </div>
-          </div>
-          <div className="mt-4">
-            <label className={styles.loginLabel} htmlFor="password">
-              Password
-            </label>
-            <div className={styles.loginFieldContainer}>
-              <input
-                className={styles.loginField}
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <KeyIcon className={styles.loginFieldIcon} />
-            </div>
-          </div>
+    <>
+      <form
+        action={async () => {
+          'use server';
+          await signIn('cognito');
+        }}
+        className={styles.loginForm}
+      >
+        <div className={styles.loginContainer}>
+          <h1 className={`${lusitana.className} ${styles.loginHeading}`}>
+            Please log in to continue.
+          </h1>
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{
+              marginTop: '1rem',
+              width: '100%',
+              backgroundColor: 'rgb(37 137 254 / 1)',
+              borderRadius: '0.5rem',
+            }}
+          >
+            Sign in with Cognito{' '}
+            <ArrowRightIcon className={styles.loginButtonIcon} />
+          </Button>
         </div>
-        <LoginButton />
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )}
-        </div>
-      </div>
-    </form>
-  );
-}
-
-function LoginButton() {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+      </form>
+    </>
   );
 }
