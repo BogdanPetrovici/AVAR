2b7d7e03181f41bea470d41e4164ae0bcf7bc7bc
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-05-14 14:55:45 +0300

    Moved pages under dashboard to simplify authentication.

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { signIn, signOut } from '@/auth';
 
 export default function SideNav() {
   return (
@@ -14,7 +15,12 @@ export default function SideNav() {
       <div className={styles.menuItemContainer}>
         <NavLinks />
         <div className={styles.menuFiller}></div>
-        <form>
+        <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
           <button className={`${styles.menuItem} ${styles.menuItemLogOff}`}>
             <PowerIcon className={styles.menuItemIcon} />
             <div className={styles.menuItemText}>Sign Out</div>
