34d020659b631b0b1fd7325aa44d22cc18bd43c8
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-05-15 21:13:52 +0300

    Separated next auth configuration from providers, as recommended by Next.js tutorial.

--- auth.ts
+++ auth.ts
@@ -1,22 +1,35 @@
 import NextAuth from 'next-auth';
 import Cognito from 'next-auth/providers/cognito';
+import Credentials from 'next-auth/providers/credentials';
+import type { Provider } from 'next-auth/providers';
+import { authConfig } from './auth.config';
+
+const providers: Provider[] = [Cognito];
+export const providerMap = [{ id: 'cognito', name: 'Cognito' }];
+
+if (process.env.NODE_ENV === 'development') {
+  providers.push(
+    Credentials({
+      credentials: {
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials, request) => {
+        if (credentials.password === 'password') {
+          return {
+            email: 'bob@alice.com',
+            name: 'Bob Alice',
+            image: 'https://avatars.githubusercontent.com/u/67470890?s=200&v=4',
+          };
+        }
+
+        return null;
+      },
+    }),
+  );
+  providerMap.push({ id: 'credentials', name: 'Credentials' });
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
-  providers: [Cognito],
+  ...authConfig,
+  providers: providers,
 });
