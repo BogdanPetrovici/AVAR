cba3bf35a0c9e7712659e80dc35adfd82b25316d
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-05-19 08:59:57 +0300

    Removed redundant credentials authentication method. Removed styling for credentials authentication method.

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,6 +1,5 @@
 import { inter, lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { KeyIcon } from '@heroicons/react/24/outline';
 import { Button } from '@mui/material';
 import styles from '@/app/ui/css/login.module.css';
 import { providerMap, signIn } from '@/auth';
@@ -13,88 +12,37 @@ export default function LoginForm() {
           Please log in to continue.
         </h1>
         {Object.values(providerMap).map((provider) => {
-          if (provider.id === 'credentials') {
-            return (
-              <form
-                key={provider.id}
-                action={async (formData) => {
-                  'use server';
-                  await signIn(provider.id, formData);
+          return (
+            <form
+              key={provider.id}
+              action={async () => {
+                'use server';
+                await signIn(provider.id);
+              }}
+              className={styles.loginForm}
+            >
+              <Button
+                variant="contained"
+                type="submit"
+                data-test="login-button"
+                sx={{
+                  marginTop: '1rem',
+                  width: '100%',
+                  height: '2.5rem',
+                  backgroundColor: 'rgb(0 112 243 / 1)',
+                  borderRadius: '0.5rem',
+                  fontWeight: '500',
+                  fontSize: '0.875rem',
+                  lineHeight: '1.25rem',
+                  fontFamily: inter.style.fontFamily,
+                  textTransform: 'none',
                 }}
-                className={styles.loginForm}
               >
-                <div>
-                  <label className={styles.loginLabel} htmlFor="password">
-                    Password
-                  </label>
-                  <div className={styles.loginFieldContainer}>
-                    <input
-                      className={styles.loginField}
-                      id="password"
-                      type="password"
-                      name="password"
-                      placeholder="Enter password"
-                      required
-                      minLength={6}
-                    />
-                    <KeyIcon className={styles.loginFieldIcon} />
-                  </div>
-                </div>
-                <Button
-                  variant="contained"
-                  type="submit"
-                  data-test="login-button-credentials"
-                  sx={{
-                    marginTop: '1rem',
-                    width: '100%',
-                    height: '2.5rem',
-                    backgroundColor: 'rgb(0 112 243 / 1)',
-                    borderRadius: '0.5rem',
-                    fontWeight: '500',
-                    fontSize: '0.875rem',
-                    lineHeight: '1.25rem',
-                    fontFamily: inter.style.fontFamily,
-                    textTransform: 'none',
-                  }}
-                >
-                  Sign in with {provider.name}{' '}
-                  <ArrowRightIcon className={styles.loginButtonIcon} />
-                </Button>
-              </form>
-            );
-          } else {
-            return (
-              <form
-                key={provider.id}
-                action={async () => {
-                  'use server';
-                  await signIn(provider.id);
-                }}
-                className={styles.loginForm}
-              >
-                <Button
-                  variant="contained"
-                  type="submit"
-                  data-test="login-button"
-                  sx={{
-                    marginTop: '1rem',
-                    width: '100%',
-                    height: '2.5rem',
-                    backgroundColor: 'rgb(0 112 243 / 1)',
-                    borderRadius: '0.5rem',
-                    fontWeight: '500',
-                    fontSize: '0.875rem',
-                    lineHeight: '1.25rem',
-                    fontFamily: inter.style.fontFamily,
-                    textTransform: 'none',
-                  }}
-                >
-                  Sign in with {provider.name}{' '}
-                  <ArrowRightIcon className={styles.loginButtonIcon} />
-                </Button>
-              </form>
-            );
-          }
+                Sign in with {provider.name}{' '}
+                <ArrowRightIcon className={styles.loginButtonIcon} />
+              </Button>
+            </form>
+          );
         })}
       </div>
     </>
--- auth.ts
+++ auth.ts
@@ -1,34 +1,10 @@
 import NextAuth from 'next-auth';
 import Cognito from 'next-auth/providers/cognito';
-import Credentials from 'next-auth/providers/credentials';
 import type { Provider } from 'next-auth/providers';
 import { authConfig } from './auth.config';
 
 const providers: Provider[] = [Cognito];
 export const providerMap = [{ id: 'cognito', name: 'Cognito' }];
-
-if (process.env.NODE_ENV === 'development') {
-  providers.push(
-    Credentials({
-      credentials: {
-        password: { label: 'Password', type: 'password' },
-      },
-      authorize: async (credentials, request) => {
-        if (credentials.password === 'password') {
-          return {
-            email: 'bob@alice.com',
-            name: 'Bob Alice',
-            image: 'https://avatars.githubusercontent.com/u/67470890?s=200&v=4',
-          };
-        }
-
-        return null;
-      },
-    }),
-  );
-  providerMap.push({ id: 'credentials', name: 'Credentials' });
-}
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: providers,
